63874f776aefe9f71a388f36e99c29149ac1d3a1
Parova522 <114745932+Parova522@users.noreply.github.com>
2022-09-30 14:33:00 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    X += 1
+})
+let X = 0
+basic.forever(function () {
+    if (X == 1) {
+        basic.showString("A")
+    } else if (X == 2) {
+        basic.showString("B")
+    } else if (X == 3) {
+        basic.showString("C")
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
